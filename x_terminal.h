3c5f82af0c2d2a942ee6c9d9a90ed557e93b984a
André Maree <ammaree@users.noreply.github.com>
2024-04-20 11:11:35 +0200

    New control structure, renamed APIs

--- x_terminal.h
+++ x_terminal.h
@@ -8,8 +8,23 @@ extern "C" {
 
 // ##################################### MACRO definitions #########################################
 
-#define	TERMINAL_DEFAULT_MAX_X				132
-#define	TERMINAL_DEFAULT_MAX_Y				48
+#define	TERMINAL_DFLT_X				132
+#define	TERMINAL_DFLT_Y				48
+#define TERMINAL_DFLT_TAB			8
+
+#define	TERMINAL_MAX_X				255
+#define	TERMINAL_MAX_Y				63
+#define TERMINAL_MAX_TAB			15
+
+#define	termWAIT_MS				100
+
+#define termFLAG_CURSOR			(1 << 0)
+#define termFLAG_LOCK			(1 << 1)
+#define termFLAG_UNLOCK			(1 << 2)
+
+#define termFLAG_ALL			(termFLAG_CURSOR|termFLAG_LOCK|termFLAG_UNLOCK)
+
+#define termBUILD_CTRL(NC,LO,UL,WAIT) ((termctrl_t) { .NoCursor = NC, .Lock = LO, .Unlock = UL, .Wait = WAIT } )
 
 // ####################################### structures ##############################################
 
@@ -19,35 +34,45 @@ typedef union {
 	};
 } terminfo_t;
 
+typedef struct __attribute__((packed)) termctrl_t {
+	union {
+		struct { bool NoCursor; bool Lock; bool Unlock; u16_t Spare:13; };
+		u16_t u16Val;
+	};
+	u16_t Wait;
+} termctrl_t;
+
+// ###################################### Public variables #########################################
 
 
 // ###################################### Public functions #########################################
 
-int	vANSIgets(char * pcBuf);
-void vANSIputs(char * pStr);
-void vANSIcursorsave(void);
-void vANSIcursorback(void);
-void vANSIcursorread(void);
+int	xTermGets(char * pcBuf, size_t Size, int Match, termctrl_t flag);
+int xTermPuts(char * pStr, termctrl_t flag);
+void vTermCursorSave(void);
+void vTermCursorBack(void);
+int xTermCursorRead(void);
 
-void vANSIclear2EOL(void);
-void vANSIclear2BOL(void);
-void vANSIclearline(void);
-void vANSIclearscreen(void);
-void vANSIclearhome(void);
+void vTermclear2EOL(void);
+void vTermclear2BOL(void);
+void vTermclearline(void);
+void vTermclearscreen(void);
+void vTermclearhome(void);
 
-void vANSIhome(void) ;
+void vTermhome(void) ;
 
-char * pcANSIattrib(char * pBuf, u8_t FG, u8_t BG) ;
-char * pcANSIlocate(char * pBuf, u8_t Row, u8_t Col) ;
+char * pcTermAttrib(char * pBuf, u8_t FG, u8_t BG) ;
+char * pcTermLocate(char * pBuf, u8_t Row, u8_t Col) ;
 
-void xANSIupdatecursor(int cChr);
-void vANSIattrib(u8_t FG, u8_t BG);
-void vANSIlocate(u8_t x, u8_t y);
+void xTermProcessChr(int cChr);
+void vTermAttrib(u8_t FG, u8_t BG);
+void vTermLocate(u8_t x, u8_t y);
 
-void vTerminalSetSize(u16_t x, u16_t y) ;
-void vTerminalGetInfo(terminfo_t * psTermInfo) ;
+void vTermSetSize(u16_t x, u16_t y) ;
+void vTermGetInfo(terminfo_t * psTermInfo) ;
+int xTermIdentify(void);
 
-void vANSItestcode(void) ;
+void vTermtestcode(void) ;
 
 #ifdef __cplusplus
 }
