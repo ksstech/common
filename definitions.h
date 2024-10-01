73a0f3796cf3d43430b2d49f00a6d184ed506eeb
André Maree <ammaree@users.noreply.github.com>
2024-10-01 18:44:31 +0200

    Add info on _Generic

--- definitions.h
+++ definitions.h
@@ -224,6 +224,37 @@ extern "C" {
 #define	BASE10						10
 #define	BASE16						16
 
+/**
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// Get the name of a type
+#define typename(x) _Generic((x),                     							  \
+                                                                                  \
+        _Bool: "_Bool",                  unsigned char: "unsigned char",          \
+         char: "char",                     signed char: "signed char",            \
+    short int: "short int",         unsigned short int: "unsigned short int",     \
+          int: "int",                     unsigned int: "unsigned int",           \
+     long int: "long int",           unsigned long int: "unsigned long int",      \
+long long int: "long long int", unsigned long long int: "unsigned long long int", \
+        float: "float",                         double: "double",                 \
+  long double: "long double",                   char *: "pointer to char",        \
+       void *: "pointer to void",                int *: "pointer to int",         \
+      default: "other")
+
+#define fmt "%20s is '%s'\n"
+int main() {
+
+  size_t s; ptrdiff_t p; intmax_t i; int ai[3] = {0}; return printf( fmt fmt fmt fmt fmt fmt fmt fmt,
+
+     "size_t", typename(s),               "ptrdiff_t", typename(p),     
+   "intmax_t", typename(i),      "character constant", typename('0'),
+ "0x7FFFFFFF", typename(0x7FFFFFFF),     "0xFFFFFFFF", typename(0xFFFFFFFF),
+"0x7FFFFFFFU", typename(0x7FFFFFFFU),  "array of int", typename(ai));
+}
+*/
+
 // true/false range tests
 #define INRANGE(x,y,z)				(((x) <= (y)) && ((y) <= (z)) ? 1 : 0)
 #define	BETWEEN(x,y,z)				(((y) >  (x)) && ((y) <  (z)) ? 1 : 0)
