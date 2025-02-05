3d65906056779e858c3bd794963d13018a1f51b9
André Maree <ammaree@users.noreply.github.com>
2025-02-06 00:11:54 +0200

    Update copyright dates

--- terminalX.c
+++ terminalX.c
@@ -1,4 +1,4 @@
-// terminalX.c - Copyright (c) 2014-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
+// terminalX.c - Copyright (c) 2014-25 Andre M. Maree / KSS Technologies (Pty) Ltd.
 
 #include "hal_platform.h"
 #include "hal_stdio.h"
