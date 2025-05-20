e555162703d68c9abfe548d4338ced592edafe85
André Maree <ammaree@users.noreply.github.com>
2025-05-20 09:09:33 +0200

    Move base complex var support into common module, common-vars.?

--- common-vars.c
+++ common-vars.c
@@ -13,6 +13,48 @@
 #define	debugPARAM					(debugFLAG & 0x4000)
 #define	debugRESULT					(debugFLAG & 0x8000)
 
+// ######################################## local constants ########################################
+
+const char * const cvSizeName[4] = { "vs08", "vs16", "vs32", "vs64" };
+const char * const cvFormName[4] = { "vfUXX", "vfIXX", "vfFXX", "vfSXX" };
+const char * const cvIndexName[cvXXX] = { "U08","U16","U32","U64","I08","I16","I32","I64","F32","F64","SXX" };
+const char cvFieldName[cvXXX][4] = { "U08","U16","U32","U64","I08","I16","I32","I64","F32","F64","SXX" };
+
+// ############################# Form / Size / Index to String support ##############################
+
+const char * pcIndex2String(cvi_e cvI) { return (cvI < cvXXX) ? cvFieldName[cvI] : "cv???"; }
+
+const char * pcForm2String(vf_e cvF) { return (cvF <= vfSXX) ? cvFormName[cvF] : "vf???"; }
+
+// ############################# Form / Size / Index to Format support #############################
+
+const char * const cvFormat[cvXXX] = {
+	"%hhu",	"%hu",	"%lu",	"%llu",
+	"%hhd",	"%hd",	"%ld",	"%lld",
+	"%f",	"%lf",	"%s"
+};
+const char * pccIndex2Format(cvi_e cvI) { return (cvI < cvXXX) ? cvFormat[cvI] : strINV_CVI; }
+
+const char * pccFormSize2Format(vf_e cvF, vs_e cvS) { return pccIndex2Format(xFormSize2Index(cvF, cvS)); }
+
+// return size correct fixed width format string
+const char * const cvFormatFix[cvX32num][3] = {
+	{ "%lu", "%ld", "%.f" },
+	{ "%3lu", "%3ld", "%3.2f" },
+	{ "%5lu", "%5ld", "%5.2f" },
+	{ "%7lu", "%7ld", "%7.2f" }
+};
+const char * pccIndex2FormatFix(cvi_e cvI, int Opt) { return (cvI == cvSXX) ? "%s" : (cvI < cvSXX) ? cvFormatFix[Opt%3][xIndex2Form(cvI)] : strINV_CVI; }
+
+// return fixed maximum width format string for x64 value
+const char * const cvFormatFix64[cvX64num][3] = {
+	{ "%llu", "%lld", "%.lf" },
+	{ "%3llu", "%3lld", "%3.2lf" },
+	{ "%5llu", "%5lld", "%5.2lf" },
+	{ "%7llu", "%7lld", "%7.2lf" }
+};
+const char * pccIndex2FormatFix64(cvi_e cvI, int Opt) { return (cvI == cvSXX) ? "%s" : (cvI < cvSXX) ? cvFormatFix64[Opt%3][xIndex2Form(cvAdjustX64(cvI))] : strINV_CVI; }
+
 // ################################# Form <> Size <> Index support #################################
 
 const u8_t cvIndexSize[] = { vs08B, vs16B, vs32B, vs64B, vs08B, vs16B, vs32B, vs64B, vs32B, vs64B };
@@ -142,6 +184,56 @@ void vx64ValueStore(x64_t X64, px_t px, cvi_e cvI) {
 	case cvF64: *px.pf64 = X64.f64; break;
 	default: assert(0);
 	}
+}
+
+// ####################################### Convert support #########################################
+
+x32_t x32ValueConvert(px_t pxSrc, cvi_e cvIS, px_t pxDst, cvi_e cvID, u8_t Scale) {
+	if (cvIS == cvID)									// nothing to be changed
+		return *pxDst.px32 = *pxSrc.px32;				// copy from source to destination, return value as is
+	vf_e vfSrc = xIndex2Form(cvIS);
+	vf_e vfDst = xIndex2Form(cvID);
+	x32_t X32 = x32ValueFetch(pxSrc, cvIS);
+	// if source is FXX and destination is NOT FXX, might have to do some scaling to
+	if (vfSrc == vfFXX && vfDst != vfFXX && Scale > 1) {
+		X32.f32 *= Scale;
+	} else {
+		IF_myASSERT(debugPARAM, Scale < 2);
+	}
+	if (vfSrc != vfDst) {								// format conversion required...
+		if (vfDst == vfUXX) {							// -> U32
+			X32.u32 = (vfSrc == vfIXX) ? X32.i32 : X32.f32;
+		} else if (vfDst == vfIXX) {					// -> I32
+			X32.i32 = (vfSrc == vfUXX) ? X32.u32 : X32.f32;
+		} else if (vfDst == vfFXX) {					// -> F32
+			X32.f32 = (vfSrc == vfUXX) ? X32.u32 : X32.i32;
+		} else {
+			IF_myASSERT(debugTRACK, 0);
+		}
+	}
+	vx32ValueStore(X32, pxDst, cvID);
+	return X32;
+}
+
+x64_t x64ValueConvert(px_t pxSrc, cvi_e cvIS, px_t pxDst, cvi_e cvID) {
+	if (cvIS == cvID)									// nothing to be changed
+		return *pxDst.px64 = *pxSrc.px64;				// copy from source to destination, return value as is
+	x64_t X64 = x64ValueFetch(pxSrc, cvIS);
+	vf_e vfSrc = xIndex2Form(cvIS);
+	vf_e vfDst = xIndex2Form(cvID);
+	if (vfSrc != vfDst) {								// format conversion required...
+		if (vfDst == vfUXX) {							// -> U64
+			X64.u64 = (vfSrc == vfIXX) ? X64.i64 : X64.f64;
+		} else if (vfDst == vfIXX) {					// -> I64
+			X64.i64 = (vfSrc == vfUXX) ? X64.u64 : X64.f64;
+		} else if (vfDst == vfFXX) {					// -> F64
+			X64.f64 = (vfSrc == vfUXX) ? X64.u64 : X64.i64;
+		} else {
+			IF_myASSERT(debugTRACK, 0);
+		}
+	}
+	vx64ValueStore(X64, pxDst, cvID);
+	return X64;
 }
 
 // ############################### Address manipulation support ####################################
--- include/common-vars.h
+++ include/common-vars.h
@@ -30,8 +30,35 @@ typedef enum   __attribute__((packed)) vf_e { vfUXX, vfIXX, vfFXX, vfSXX } vf_e;
 
 // ####################################### Structures ##############################################
 
+// ######################################### Global variables ######################################
+
+extern const char * const cvSizeName[];
+extern const char * const cvFormName[];
+extern const char * const cvIndexName[];
+
+// ############################# Form / Size / Index to String support ##############################
+
+const char * pcIndex2String(cvi_e cvI);
+const char * pcForm2String(vf_e cvF);
+
+// ############################# Form / Size / Index to Format support #############################
+
+const char * pccIndex2Format(cvi_e cvI);
+const char * pccFormSize2Format(vf_e cvF, vs_e cvS);
+
+enum { cvX32free, cvX32fix3, cvX32fix5, cvX32fix7, cvX32num };
+const char * pccIndex2FormatFix(cvi_e cvI, int Opt);
+
+enum { cvX64free, cvX64fix3, cvX64fix5, cvX64fix7, cvX64num };
+const char * pccIndex2FormatFix64(cvi_e cvI, int Opt);
+
 // ################################# Form <> Size <> Index support #################################
 
+/**
+ * @brief	
+ * @param[in]	
+ * @return			
+ */
 vs_e xIndex2Size(cvi_e cvI);
 u32_t xIndex2Bytes(cvi_e cvI);
 vf_e xIndex2Form(cvi_e cvI);
@@ -78,6 +105,27 @@ x64_t x64ValueFetch(px_t px, cvi_e cvI);
 void vx32ValueStore(x32_t X32, px_t px, cvi_e cvI);
 
 void vx64ValueStore(x64_t x64Val, px_t px, cvi_e cvI);
+
+/**
+ * @brief	Convert (up/same/down size) source value/format to x32_t destination value/format
+ * @param	pxSrc - source address
+ * @param	cvIS - source value format
+ * @param	pxDst - destination address
+ * @param	cviD - destination value format
+ * @param	Scale - optional scale factor for FXX -> IXX/UXX conversion, preserve some fraction
+ * @return	32 bit value in requested format
+ */
+x32_t x32ValueConvert(px_t pxSrc, cvi_e cvIS, px_t pxDst, cvi_e cvID, u8_t Scale);
+
+/**
+ * @brief	Convert (up/same/down size) source value/format to x64_t destination value/format
+ * @param	pxSrc - source address
+ * @param	cvIS - source value format
+ * @param	pxDst - destination address
+ * @param	cviD - destination value format
+ * @return	64 bit value in requested format
+ */
+x64_t x64ValueConvert(px_t pxSrc, cvi_e cvIS, px_t pxDst, cvi_e cvID);
 
 // ############################### Address manipulation support ####################################
 
